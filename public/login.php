c667f2c655a804702f353243a4d60d473919f4b5
MattyxD24 <mathiasbc97@gmail.com>
2023-10-10 10:46:01 +0200

    added more stuff

---
+++ public/login.php
@@ -0,0 +1,7 @@
+<?php
+
+require("../src/connection.php");
+require("mainBG.php");
+
+?>
+<div class="w-40 h-40 bg-white"></div>
--- src/connection.php
+++ src/connection.php
@@ -1,6 +1,6 @@
 <?php
 
-require("./constants.php");
+require("constants.php");
 
 $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS);
 
