dc108950d48d576a1262f8ad0327254c80672676
EmilseBilse <bandittenxd@gmail.com>
2023-10-26 17:34:09 +0200

    add this to constants yes
    define('BASE_URL', '/work/DWP2023');

--- views/login.php
+++ views/login.php
@@ -4,16 +4,21 @@ require("../db/connection.php");
 require("mainBG.php");
 
 ?>
-<div class="text-white">Welcome to <span class="text-red-600">Bread</span>Tube, a <span class="text-red-600">rising</span> community.</div>
-<form>
-    <label class="text-red-600" for="username">Username or email:</label><br>
-    <input class="std_input" type="text" name="username">
-    <br>
-    <label class="text-red-600" for="password">Password:</label><br>
-    <input class="std_input" type="password" name="password">
-    <br>
-    <button type="submit" class="std_button text-white font-bold py-2 px-4 rounded">
-        <span class="text-red-600">Log </span><span>In</span>
-    </button>
-
-</form>
+<article class="flex justify-center items-center flex-col">
+    <div class="text-white text-2xl font-bold">Welcome to <span class="text-red-600">Bread</span>Tube, a <span class="text-red-600">rising</span> community.</div>
+    <form>
+        <div class="py-3 flex flex-col">
+            <label class="text-red-600 text-xl" for="username">Username or email:</label><br>
+            <input class="std_input w-[500px]" type="text" name="username">
+        </div>
+        <div class="py-3 flex flex-col">
+            <label class="text-red-600 text-xl" for="password">Password:</label><br>
+            <input class="std_input w-[500px]" type="password" name="password">
+        </div>
+        <div class="py-3 flex flex-col">
+            <button type="submit" class="std_button text-white font-bold py-2 px-4 rounded">
+                <span class="text-red-600">Log </span><span>In</span>
+            </button>
+        </div>
+    </form> 
+</article>
