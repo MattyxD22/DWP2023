c870e33f4d4aa8d7c6fb8c27cdaecc117f90afb2
EmilseBilse <bandittenxd@gmail.com>
2023-10-26 18:33:56 +0200

    login with both email and username

--- models/UserModel.php
+++ models/UserModel.php
@@ -9,8 +9,7 @@ class UserModel extends BaseModel
     {
     }
 
-    function create($username, $email, $password)
-    {
+    function create($username, $email, $password) {
         try {
             $cxn = parent::connectToDB();
 
@@ -32,16 +31,27 @@ class UserModel extends BaseModel
         }
     }
 
-    function login($username, $password)
-    {
+    function login($username, $password) {
         try {
             $cxn = parent::connectToDB();
-            $statement = "SELECT password FROM usertable WHERE username = :username LIMIT 1";
+
+            // First, try treating the input as a username
+            $statement = "SELECT password FROM usertable WHERE username = :input LIMIT 1";
             $handle = $cxn->prepare($statement);
-            $handle->bindParam(':username', $username);
+            $handle->bindParam(':input', $username);
             $handle->execute();
             $result = $handle->fetch(\PDO::FETCH_ASSOC);
 
+            // If no match was found for username, try treating the input as an email
+            if (!$result) {
+                $statement = "SELECT password FROM usertable WHERE email = :input LIMIT 1";
+                $handle = $cxn->prepare($statement);
+                $handle->bindParam(':input', $username);
+                $handle->execute();
+                $result = $handle->fetch(\PDO::FETCH_ASSOC);
+            }
+
+            // Verify the password
             if ($result && password_verify($password, $result['password'])) {
                 return true;
             } else {
