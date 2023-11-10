4072bbec1ae15a918f145ac34d07bf2a0558be93
MattyxD24 <mathiasbc97@gmail.com>
2023-11-10 10:37:22 +0100

    plz

--- db/createDB.sql
+++ db/createDB.sql
@@ -56,8 +56,8 @@ CREATE TABLE FollowingTable (
     ID INT(11) AUTO_INCREMENT PRIMARY KEY,
     UserID INT(11),
     FollowingID INT(11),
-    FOREIGN KEY(UserID) REFERENCES usertable(UserID),
-    FOREIGN KEY(FollowingID) REFERENCES usertable(UserID)
+    FOREIGN KEY(UserID) REFERENCES UserTable(UserID),
+    FOREIGN KEY(FollowingID) REFERENCES UserTable(UserID)
 ) ENGINE = INNODB;
 
 
@@ -66,7 +66,7 @@ CREATE TABLE LikesTable(
     UserID INT(11),
     PostID INT(11),
     Type INT(11),
-    FOREIGN KEY(UserID) REFERENCES usertable(UserID),
+    FOREIGN KEY(UserID) REFERENCES UserTable(UserID),
     FOREIGN KEY(PostID) REFERENCES postTable(PostID)
 ) ENGINE = INNODB;
 
@@ -75,54 +75,54 @@ CREATE TABLE RepostTable(
     ID INT(11) AUTO_INCREMENT PRIMARY KEY,
     UserID INT(11),
     PostID INT(11),
-    FOREIGN KEY(UserID) REFERENCES usertable(UserID),
+    FOREIGN KEY(UserID) REFERENCES UserTable(UserID),
     FOREIGN KEY(PostID) REFERENCES postTable(PostID)
 ) ENGINE = INNODB;
 
-ALTER TABLE usertable ADD FOREIGN KEY (MediaID) REFERENCES mediatable(MediaID);
+ALTER TABLE UserTable ADD FOREIGN KEY (MediaID) REFERENCES MediaTable(MediaID);
 ALTER TABLE PostTable ADD FOREIGN KEY (CategoryID) REFERENCES CategoryPostTable(ID);
 
-insert into usertable (username, FName, LName, email) values ('jflipsen0', 'Jess', 'Flipsen', 'jflipsen0@latimes.com');
-insert into usertable (username, FName, LName, email) values ('fstanislaw1', 'Florette', 'Stanislaw', 'fstanislaw1@cam.ac.uk');
-insert into usertable (username, FName, LName, email) values ('bspurret2', 'Bridie', 'Spurret', 'bspurret2@cafepress.com');
-insert into usertable (username, FName, LName, email) values ('lwem3', 'Laurene', 'Wem', 'lwem3@cornell.edu');
-insert into usertable (username, FName, LName, email) values ('lwoffenden4', 'Logan', 'Woffenden', 'lwoffenden4@wikia.com');
-insert into usertable (username, FName, LName, email) values ('ofrounks5', 'Odey', 'Frounks', 'ofrounks5@hhs.gov');
-insert into usertable (username, FName, LName, email) values ('othames6', 'Osborn', 'Thames', 'othames6@webeden.co.uk');
-insert into usertable (username, FName, LName, email) values ('bnewey7', 'Brita', 'Newey', 'bnewey7@icq.com');
-insert into usertable (username, FName, LName, email) values ('ecarreck8', 'Estell', 'Carreck', 'ecarreck8@hubpages.com');
-insert into usertable (username, FName, LName, email) values ('awinthrop9', 'Aridatha', 'Winthrop', 'awinthrop9@bravesites.com');
-insert into usertable (username, FName, LName, email) values ('ggieroka', 'Gordon', 'Gierok', 'ggieroka@netlog.com');
-insert into usertable (username, FName, LName, email) values ('rknuttonb', 'Rolfe', 'Knutton', 'rknuttonb@foxnews.com');
-insert into usertable (username, FName, LName, email) values ('gromic', 'Gale', 'Romi', 'gromic@nbcnews.com');
-insert into usertable (username, FName, LName, email) values ('cwinslowd', 'Cly', 'Winslow', 'cwinslowd@woothemes.com');
-insert into usertable (username, FName, LName, email) values ('kmatushenkoe', 'Karalynn', 'Matushenko', 'kmatushenkoe@t.co');
-insert into usertable (username, FName, LName, email) values ('ghunef', 'Griz', 'Hune', 'ghunef@shutterfly.com');
-insert into usertable (username, FName, LName, email) values ('rbardeyg', 'Reagan', 'Bardey', 'rbardeyg@people.com.cn');
-insert into usertable (username, FName, LName, email) values ('nbollardh', 'Normie', 'Bollard', 'nbollardh@comcast.net');
-insert into usertable (username, FName, LName, email) values ('bmelhuishi', 'Buddie', 'Melhuish', 'bmelhuishi@intel.com');
-insert into usertable (username, FName, LName, email) values ('aparsonagej', 'Alanson', 'Parsonage', 'aparsonagej@typepad.com');
-insert into usertable (username, FName, LName, email) values ('bathyk', 'Brook', 'Athy', 'bathyk@vkontakte.ru');
-insert into usertable (username, FName, LName, email) values ('ialphegel', 'Ingelbert', 'Alphege', 'ialphegel@mayoclinic.com');
-insert into usertable (username, FName, LName, email) values ('tgotcherm', 'Tedda', 'Gotcher', 'tgotcherm@earthlink.net');
-insert into usertable (username, FName, LName, email) values ('mbendsonn', 'Marris', 'Bendson', 'mbendsonn@sphinn.com');
-insert into usertable (username, FName, LName, email) values ('aitzkovwitcho', 'Archibaldo', 'Itzkovwitch', 'aitzkovwitcho@aol.com');
+insert into UserTable (Username, FName, LName, Email) values ('jflipsen0', 'Jess', 'Flipsen', 'jflipsen0@latimes.com');
+insert into UserTable (Username, FName, LName, Email) values ('fstanislaw1', 'Florette', 'Stanislaw', 'fstanislaw1@cam.ac.uk');
+insert into UserTable (Username, FName, LName, Email) values ('bspurret2', 'Bridie', 'Spurret', 'bspurret2@cafepress.com');
+insert into UserTable (Username, FName, LName, Email) values ('lwem3', 'Laurene', 'Wem', 'lwem3@cornell.edu');
+insert into UserTable (Username, FName, LName, Email) values ('lwoffenden4', 'Logan', 'Woffenden', 'lwoffenden4@wikia.com');
+insert into UserTable (Username, FName, LName, Email) values ('ofrounks5', 'Odey', 'Frounks', 'ofrounks5@hhs.gov');
+insert into UserTable (Username, FName, LName, Email) values ('othames6', 'Osborn', 'Thames', 'othames6@webeden.co.uk');
+insert into UserTable (Username, FName, LName, Email) values ('bnewey7', 'Brita', 'Newey', 'bnewey7@icq.com');
+insert into UserTable (Username, FName, LName, Email) values ('ecarreck8', 'Estell', 'Carreck', 'ecarreck8@hubpages.com');
+insert into UserTable (Username, FName, LName, Email) values ('awinthrop9', 'Aridatha', 'Winthrop', 'awinthrop9@bravesites.com');
+insert into UserTable (Username, FName, LName, Email) values ('ggieroka', 'Gordon', 'Gierok', 'ggieroka@netlog.com');
+insert into UserTable (Username, FName, LName, Email) values ('rknuttonb', 'Rolfe', 'Knutton', 'rknuttonb@foxnews.com');
+insert into UserTable (Username, FName, LName, Email) values ('gromic', 'Gale', 'Romi', 'gromic@nbcnews.com');
+insert into UserTable (Username, FName, LName, Email) values ('cwinslowd', 'Cly', 'Winslow', 'cwinslowd@woothemes.com');
+insert into UserTable (Username, FName, LName, Email) values ('kmatushenkoe', 'Karalynn', 'Matushenko', 'kmatushenkoe@t.co');
+insert into UserTable (Username, FName, LName, Email) values ('ghunef', 'Griz', 'Hune', 'ghunef@shutterfly.com');
+insert into UserTable (Username, FName, LName, Email) values ('rbardeyg', 'Reagan', 'Bardey', 'rbardeyg@people.com.cn');
+insert into UserTable (Username, FName, LName, Email) values ('nbollardh', 'Normie', 'Bollard', 'nbollardh@comcast.net');
+insert into UserTable (Username, FName, LName, Email) values ('bmelhuishi', 'Buddie', 'Melhuish', 'bmelhuishi@intel.com');
+insert into UserTable (Username, FName, LName, Email) values ('aparsonagej', 'Alanson', 'Parsonage', 'aparsonagej@typepad.com');
+insert into UserTable (Username, FName, LName, Email) values ('bathyk', 'Brook', 'Athy', 'bathyk@vkontakte.ru');
+insert into UserTable (Username, FName, LName, Email) values ('ialphegel', 'Ingelbert', 'Alphege', 'ialphegel@mayoclinic.com');
+insert into UserTable (Username, FName, LName, Email) values ('tgotcherm', 'Tedda', 'Gotcher', 'tgotcherm@earthlink.net');
+insert into UserTable (Username, FName, LName, Email) values ('mbendsonn', 'Marris', 'Bendson', 'mbendsonn@sphinn.com');
+insert into UserTable (Username, FName, LName, Email) values ('aitzkovwitcho', 'Archibaldo', 'Itzkovwitch', 'aitzkovwitcho@aol.com');
 
 
 
-INSERT INTO categorytable(categorytable.Title) VALUES ("Homemade");
-INSERT INTO categorytable(categorytable.Title) VALUES ("Amateur");
-INSERT INTO categorytable(categorytable.Title) VALUES ("Professionals");
-insert into categorytable(categorytable.Title) VALUES("Category 1");
-insert into categorytable(categorytable.Title) VALUES("Category 2");
-insert into categorytable(categorytable.Title) VALUES("Category 3");
-insert into categorytable(categorytable.Title) VALUES("Category 4");
-insert into categorytable(categorytable.Title) VALUES("Category 5");
+INSERT INTO CategoryTable(CategoryTable.Title) VALUES ("Homemade");
+INSERT INTO CategoryTable(CategoryTable.Title) VALUES ("Amateur");
+INSERT INTO CategoryTable(CategoryTable.Title) VALUES ("Professionals");
+insert into CategoryTable(CategoryTable.Title) VALUES("Category 1");
+insert into CategoryTable(CategoryTable.Title) VALUES("Category 2");
+insert into CategoryTable(CategoryTable.Title) VALUES("Category 3");
+insert into CategoryTable(CategoryTable.Title) VALUES("Category 4");
+insert into CategoryTable(CategoryTable.Title) VALUES("Category 5");
 
 DELIMITER //
 CREATE PROCEDURE addNewPost(IN Description VARCHAR(500), IN UserID INT(11), IN Title VARCHAR(50))
 BEGIN
-INSERT INTO posttable(posttable.Description, posttable.CreatedBy, postTable.Title) VALUES(Description, UserID, Title);
+INSERT INTO PostTable(PostTable.Description, PostTable.CreatedBy, postTable.Title) VALUES(Description, UserID, Title);
 SELECT LAST_INSERT_ID() AS 'PostID';
 END //
 DELIMITER ;
@@ -130,20 +130,20 @@ DELIMITER ;
 DELIMITER //
 CREATE PROCEDURE getFeed()
 BEGIN
-SELECT posttable.PostID, posttable.Description, posttable.CreatedBy, posttable.Title, usertable.Username, usertable.UserID FROM posttable LEFT JOIN usertable ON usertable.UserID = posttable.CreatedBy WHERE posttable.ParentID IS NULL;
+SELECT PostTable.PostID, PostTable.Description, PostTable.CreatedBy, PostTable.Title, UserTable.Username, UserTable.UserID FROM PostTable LEFT JOIN UserTable ON UserTable.UserID = PostTable.CreatedBy WHERE PostTable.ParentID IS NULL;
 END //
 DELIMITER ;
 
-DELIMITER ;
+
 
 DELIMITER //
 CREATE PROCEDURE getPost(IN postID INT(11))
 BEGIN 
 
-SELECT posttable.PostID, posttable.Title, posttable.Description, posttable.CreatedDate, usertable.Username
-FROM posttable 
-LEFT JOIN usertable ON usertable.UserID = posttable.CreatedBy 
-WHERE posttable.PostID = postID;
+SELECT PostTable.PostID, PostTable.Title, PostTable.Description, PostTable.CreatedDate, UserTable.Username
+FROM PostTable 
+LEFT JOIN UserTable ON UserTable.UserID = PostTable.CreatedBy 
+WHERE PostTable.PostID = postID;
 
 END //
 
@@ -153,7 +153,7 @@ DELIMITER //
 CREATE PROCEDURE createComment(IN postID INT(11), IN userComment VARCHAR(500), IN userID INT(11))
 BEGIN 
 
-INSERT INTO posttable(posttable.ParentID, posttable.Description, posttable.CreatedDate, posttable.CreatedBy) VALUES (postID, userComment, NOW(), userID);
+INSERT INTO PostTable(PostTable.ParentID, PostTable.Description, PostTable.CreatedDate, PostTable.CreatedBy) VALUES (postID, userComment, NOW(), userID);
 
 END //
 
@@ -165,12 +165,12 @@ BEGIN
 
 SET @Exists = NULL;
 
-SELECT likestable.PostID INTO @Exists FROM likestable WHERE likestable.PostID = PostID AND likestable.UserID = UserID; 
+SELECT LikesTable.PostID INTO @Exists FROM LikesTable WHERE LikesTable.PostID = PostID AND LikesTable.UserID = UserID; 
 
 IF @Exists IS NULL THEN
-    INSERT INTO likestable(likestable.PostID, likestable.UserID, likestable.Type) VALUES (PostID, UserID, Type);
+    INSERT INTO LikesTable(LikesTable.PostID, LikesTable.UserID, LikesTable.Type) VALUES (PostID, UserID, Type);
 ELSE 
-    UPDATE likestable SET likestable.Type = Type WHERE likestable.postID = PostID AND likestable.UserID = UserID;
+    UPDATE LikesTable SET LikesTable.Type = Type WHERE LikesTable.postID = PostID AND LikesTable.UserID = UserID;
 END IF;
 
 
@@ -183,7 +183,7 @@ DELIMITER //
 CREATE PROCEDURE deleteFromLikesTable(IN postID INT(11), IN UserID INT(11))
 BEGIN 
 
-DELETE FROM likestable WHERE likestable.PostID = PostID AND likestable.UserID = UserID;
+DELETE FROM LikesTable WHERE LikesTable.PostID = PostID AND LikesTable.UserID = UserID;
 
 
 END //
@@ -196,19 +196,19 @@ CREATE PROCEDURE getComments(IN postID INT(11))
 BEGIN 
 
 SELECT DISTINCT
-    posttable.PostID,
-    posttable.ParentID,
-    posttable.Description,
-    posttable.CreatedDate,
-    posttable.CreatedBy,
-    usertable.Username,
-    (SELECT COUNT(*) FROM likestable WHERE likestable.Type = 1 AND likestable.PostID = posttable.ParentID) AS 'Likes',
-    (SELECT COUNT(*) FROM likestable WHERE likestable.Type = 2 AND likestable.PostID = posttable.ParentID) AS 'Dislikes'
+    PostTable.PostID,
+    PostTable.ParentID,
+    PostTable.Description,
+    PostTable.CreatedDate,
+    PostTable.CreatedBy,
+    UserTable.Username,
+    (SELECT COUNT(*) FROM LikesTable WHERE LikesTable.Type = 1 AND LikesTable.PostID = PostTable.ParentID) AS 'Likes',
+    (SELECT COUNT(*) FROM LikesTable WHERE LikesTable.Type = 2 AND LikesTable.PostID = PostTable.ParentID) AS 'Dislikes'
 FROM
-    posttable
-LEFT JOIN usertable ON usertable.UserID = posttable.CreatedBy
+    PostTable
+LEFT JOIN UserTable ON UserTable.UserID = PostTable.CreatedBy
 WHERE
-    posttable.ParentID = PostID
+    PostTable.ParentID = PostID
 
 UNION
 
@@ -219,15 +219,15 @@ SELECT DISTINCT
     p2.CreatedDate,
     p2.CreatedBy,
     u2.Username,
-    (SELECT COUNT(*) FROM likestable WHERE likestable.Type = 1 AND likestable.PostID = p2.ParentID) AS 'Likes',
-    (SELECT COUNT(*) FROM likestable WHERE likestable.Type = 2 AND likestable.PostID = p2.ParentID) AS 'Dislikes'
+    (SELECT COUNT(*) FROM LikesTable WHERE LikesTable.Type = 1 AND LikesTable.PostID = p2.ParentID) AS 'Likes',
+    (SELECT COUNT(*) FROM LikesTable WHERE LikesTable.Type = 2 AND LikesTable.PostID = p2.ParentID) AS 'Dislikes'
 FROM
-    posttable
-LEFT JOIN usertable ON usertable.UserID = posttable.CreatedBy
-LEFT JOIN posttable p2 ON p2.ParentID = posttable.PostID
-LEFT JOIN usertable u2 ON u2.UserID = p2.CreatedBy
+    PostTable
+LEFT JOIN UserTable ON UserTable.UserID = PostTable.CreatedBy
+LEFT JOIN PostTable p2 ON p2.ParentID = PostTable.PostID
+LEFT JOIN UserTable u2 ON u2.UserID = p2.CreatedBy
 WHERE
-    posttable.ParentID = PostID;
+    PostTable.ParentID = PostID;
 
 END //
 
@@ -237,7 +237,7 @@ DELIMITER //
 CREATE PROCEDURE getCategories()
 BEGIN 
 
-SELECT * FROM categorytable; 
+SELECT * FROM CategoryTable; 
 
 
 END //
@@ -248,7 +248,7 @@ DELIMITER //
 CREATE PROCEDURE addFileToPost(IN Type INT(11), IN PostID INT(11), IN FileData LONGBLOB)
 BEGIN 
 
-INSERT INTO mediatable(mediatable.MediaType, mediatable.UploadDate, mediatable.PostID, mediatable.ImgData) VALUES (Type, NOW(), PostID, FileData);
+INSERT INTO MediaTable(MediaTable.MediaType, MediaTable.UploadDate, MediaTable.PostID, MediaTable.ImgData) VALUES (Type, NOW(), PostID, FileData);
 
 
 END //
@@ -270,12 +270,12 @@ DELIMITER //
 CREATE PROCEDURE getPostsInCategory(IN CategoryID INT(11))
 BEGIN 
 
-SELECT posttable.PostID, posttable.CreatedDate, posttable.CreatedBy, posttable.Title, usertable.Username, (SELECT COUNT(*) FROM likestable WHERE likestable.PostID = posttable.PostID AND likestable.Type = 1) AS 'Likes', (SELECT COUNT(*) FROM likestable WHERE likestable.PostID = posttable.PostID AND likestable.Type = 2) AS 'Dislikes', (SELECT COUNT(posttable.Description) FROM posttable WHERE posttable.ParentID IS NOT NULL AND posttable.PostID = categoryposttable.PostID) AS 'Comments', mediatable.ImgData FROM categorytable
-LEFT JOIN categoryposttable ON categoryposttable.CategoryID = categorytable.CategoryID
-LEFT JOIN posttable ON posttable.PostID = categoryposttable.PostID
-LEFT JOIN usertable ON usertable.UserID = posttable.CreatedBy
-LEFT JOIN mediatable ON mediatable.PostID = posttable.PostID
-WHERE categorytable.CategoryID = CategoryID;
+SELECT PostTable.PostID, PostTable.CreatedDate, PostTable.CreatedBy, PostTable.Title, UserTable.Username, (SELECT COUNT(*) FROM LikesTable WHERE LikesTable.PostID = PostTable.PostID AND LikesTable.Type = 1) AS 'Likes', (SELECT COUNT(*) FROM LikesTable WHERE LikesTable.PostID = PostTable.PostID AND LikesTable.Type = 2) AS 'Dislikes', (SELECT COUNT(PostTable.Description) FROM PostTable WHERE PostTable.ParentID IS NOT NULL AND PostTable.PostID = CategoryPostTable.PostID) AS 'Comments', MediaTable.ImgData FROM CategoryTable
+LEFT JOIN CategoryPostTable ON CategoryPostTable.CategoryID = CategoryTable.CategoryID
+LEFT JOIN PostTable ON PostTable.PostID = CategoryPostTable.PostID
+LEFT JOIN UserTable ON UserTable.UserID = PostTable.CreatedBy
+LEFT JOIN MediaTable ON MediaTable.PostID = PostTable.PostID
+WHERE CategoryTable.CategoryID = CategoryID;
 
 END //
 
@@ -285,7 +285,7 @@ DELIMITER //
 CREATE PROCEDURE getPostImgs(IN PostID INT(11))
 BEGIN 
 
-SELECT mediatable.ImgData, mediatable.MediaID FROM mediatable WHERE mediatable.PostID = PostID;
+SELECT MediaTable.ImgData, MediaTable.MediaID FROM MediaTable WHERE MediaTable.PostID = PostID;
 
 END //
 
@@ -296,10 +296,10 @@ DELIMITER //
 CREATE PROCEDURE getUncatorizedPosts()
 BEGIN 
 
-SELECT posttable.PostID, posttable.CreatedDate, posttable.CreatedBy, posttable.Title, usertable.Username, mediatable.ImgData, (SELECT COUNT(*) FROM likestable WHERE likestable.PostID = posttable.PostID AND likestable.Type = 1) AS 'Likes', (SELECT COUNT(*) FROM likestable WHERE likestable.PostID = posttable.PostID AND likestable.Type = 2) AS 'Dislikes' FROM PostTable
-LEFT JOIN usertable ON usertable.UserID = posttable.CreatedBy
-LEFT JOIN mediatable ON mediatable.PostID = posttable.PostID
-WHERE posttable.CategoryID IS NULL;
+SELECT PostTable.PostID, PostTable.CreatedDate, PostTable.CreatedBy, PostTable.Title, UserTable.Username, MediaTable.ImgData, (SELECT COUNT(*) FROM LikesTable WHERE LikesTable.PostID = PostTable.PostID AND LikesTable.Type = 1) AS 'Likes', (SELECT COUNT(*) FROM LikesTable WHERE LikesTable.PostID = PostTable.PostID AND LikesTable.Type = 2) AS 'Dislikes' FROM PostTable
+LEFT JOIN UserTable ON UserTable.UserID = PostTable.CreatedBy
+LEFT JOIN MediaTable ON MediaTable.PostID = PostTable.PostID
+WHERE PostTable.CategoryID IS NULL;
 
 END //
 
